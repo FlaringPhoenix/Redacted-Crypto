1bd0d04e5595cb89c56b8ae2c3e129fa29895026
FlaringPhoenix <joshmeubanks@gmail.com>
2021-07-29 18:23:48 -0500

    fix: tests

--- tests/index.js
+++ tests/index.js
@@ -6,7 +6,7 @@ const port = 3000;
 let BTC = new RedactedCrypto.Watcher('BTCUSDT', 'BTC', 'USDT', 'binance');
 let ETH = new RedactedCrypto.Watcher('ETHUSDT', 'ETH', 'USDT', 'binance');
 
-let client = new RedactedCrypto.Client([BTC, ETH], port, false);
+let client = new RedactedCrypto.Client([BTC, ETH], port, 10);
 
 BTC.on('trade', (trade) => {
     console.log(trade);
